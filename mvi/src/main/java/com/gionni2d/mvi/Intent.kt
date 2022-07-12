4d621a129f332923de2c0c8259895198cf50e57f
gionni2d <giovanni@conio.com>
2022-07-12 18:07:02 +0200

    feat: added Event marker to distinguish business logic interation towars the model to user interaction towards the model (Intents)

--- mvi/src/main/java/com/gionni2d/mvi/Intent.kt
+++ mvi/src/main/java/com/gionni2d/mvi/Intent.kt
@@ -1,3 +1,18 @@
 package com.gionni2d.mvi
 
-interface Intent
+/**
+ * Interaction towards the [model][Model], which can lead to a change
+ * of [state][State] or to a side effect.
+ */
+sealed interface Action
+
+/**
+ * Interaction from the user towards the [model][Model]
+ * (translation from the user's mental model to the app model)
+ */
+interface Intent: Action
+
+/**
+ * Interaction from a layer other than the presentation layer towards the [model][Model]
+ */
+interface Event: Action
--- mvi/src/main/java/com/gionni2d/mvi/Model.kt
+++ mvi/src/main/java/com/gionni2d/mvi/Model.kt
@@ -7,17 +7,17 @@ interface State
 /**
  * constraint: pure function
  */
-fun interface Reducer<S : State, I : Intent> {
-    fun reduce(state: S, intent: I): S
+fun interface Reducer<S : State, A : Action> {
+    fun reduce(state: S, action: A): S
 }
 
 fun interface Model<S : State, I : Intent> {
     fun subscribeTo(intents: Flow<I>): StateFlow<S>
 }
 
-inline fun <S : State, I : Intent, reified SI : I> Flow<I>.applyReducer(
+inline fun <S : State, A : Action, reified SA : A> Flow<A>.applyReducer(
     store: Store<S>,
-    reducer: Reducer<S, SI>
-) : Flow<I> = this.onEach { intent ->
-    (intent as? SI)?.let { store.update { reducer.reduce(it, intent) } }
+    reducer: Reducer<S, SA>
+) : Flow<A> = this.onEach { action ->
+    (action as? SA)?.let { store.update { reducer.reduce(it, action) } }
 }
