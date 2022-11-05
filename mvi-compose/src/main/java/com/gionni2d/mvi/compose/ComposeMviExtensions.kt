7e02581af101108ec0298cb7d8c3b0c02f50a1a2
gionni2d <giovanni@conio.com>
2022-11-05 15:28:28 +0100

    fix: same intent not triggered twice

--- mvi-compose/src/main/java/com/gionni2d/mvi/compose/ComposeMviExtensions.kt
+++ mvi-compose/src/main/java/com/gionni2d/mvi/compose/ComposeMviExtensions.kt
@@ -2,18 +2,26 @@ package com.gionni2d.mvi.compose
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import com.gionni2d.mvi.Intent
 import com.gionni2d.mvi.Model
 import com.gionni2d.mvi.State
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.launch
 
-fun <I : Intent> intentsFlowForCompose(): Pair<(I) -> Unit, Flow<I>> {
-    val stateFlow = MutableStateFlow<I?>(null)
-    val callback: (I) -> Unit = { stateFlow.value = it }
-    return callback to stateFlow.filterNotNull()
+private fun <I : Intent> intentsFlowForCompose(
+    scope: CoroutineScope
+): Pair<(I) -> Unit, Flow<I>> {
+    val intentFlow = MutableSharedFlow<I>()
+    val intentCallback: (I) -> Unit = { intent ->
+        scope.launch {
+            intentFlow.emit(intent)
+        }
+    }
+    return intentCallback to intentFlow
 }
 
 data class MviComponents<S : State, I : Intent>(
@@ -22,11 +30,12 @@ data class MviComponents<S : State, I : Intent>(
 )
 
 @Composable
-fun <S: State, I: Intent> rememberMviComponent(
-    model: Model<S, I>
-) : MviComponents<S, I> {
+fun <S : State, I : Intent> rememberMviComponent(
+    model: Model<S, I>,
+    scope: CoroutineScope = rememberCoroutineScope()
+): MviComponents<S, I> {
     return remember {
-        val (onIntent, intentFlow) = intentsFlowForCompose<I>()
+        val (onIntent, intentFlow) = intentsFlowForCompose<I>(scope)
         val stateFlow = model.subscribeTo(intentFlow)
         MviComponents(
             stateFlow = stateFlow,
