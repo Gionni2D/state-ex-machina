f0758de9cafbd6ac46aadbcc7e28e73d2197a84a
Massimo Petrogalli <>
2023-11-15 23:08:36 +0100

    feat: added new module mvi-viewmodel, added ViewModelExtensions

---
+++ mvi-viewmodel/src/main/java/com/gionni2d/mvi/viewmodel/ViewModelExtensions.kt
@@ -0,0 +1,33 @@
+package com.gionni2d.mvi.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.gionni2d.mvi.dsl.IntentStateMachineScope
+import com.gionni2d.mvi.foundation.Intent
+import com.gionni2d.mvi.foundation.State
+import com.gionni2d.mvi.foundation.Store
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.StateFlow
+import com.gionni2d.mvi.dsl.stateMachine
+
+fun <S : State, I : Intent> ViewModel.stateMachine(
+    initialState: S,
+    intents: Flow<I>,
+    block: IntentStateMachineScope<S, I>.() -> Unit
+): StateFlow<S> = stateMachine(
+    initialState = initialState,
+    intents = intents,
+    coroutineScope = viewModelScope,
+    block = block,
+)
+
+fun <S : State, I : Intent> ViewModel.stateMachine(
+    store: Store<S>,
+    intents: Flow<I>,
+    block: IntentStateMachineScope<S, I>.() -> Unit
+): StateFlow<S> = stateMachine(
+    store = store,
+    intents = intents,
+    coroutineScope = viewModelScope,
+    block = block,
+)
